d4500d95ed5f284dc4bcf7be8a9663d1de075d86
熊晓英 <xiongxiaoying@xiongxiingdeMBP.lan>
2019-12-07 18:25:20 +0800

    input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function InputNumber () {
+  console.log(arguments[0])
+  const InputValue = arguments[0].value
+  const DefaultValue = arguments[0].defaultValue
+  const [innervalue, setInnerValue] = useState(DefaultValue || InputValue)
+  const OnChange = arguments[0].onChange || function () { }
+  const InputPlaceholder = arguments[0].placeholder || "请输入文字"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input placeholder={InputPlaceholder} value={InputValue ? InputValue : innervalue} onChange={(e) => {
+        if (DefaultValue) {
+          setInnerValue(e.target.value)
+        } else {
+          OnChange(e)
+        }
+      }} />
     </div>
-  );
+  )
 }
+function App () {
+  const [value, setValue] = useState('aaa')
+  const [Placeholder, setPlaceholder] = useState('我是placeholder')
+  return (
+    <div>
+      <InputNumber
+        value={value}
+        onChange={e => { setValue(e.target.value) }}
+        placeholder={Placeholder}
+      />
+      <hr />
+      <InputNumber
+        onChange={e => { setValue(e.target.value) }}
+        placeholder={Placeholder}
+        defaultValue={value}
+      />
+    </div>
+  )
+}
+
 
 export default App;
